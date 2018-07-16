e47eb19f33c2af208420016dcd4e412a996c8a8f
Amine <sghir.ma@gmail.com>
2018-07-16 14:33:27 +0200

    added view

--- front/index.php
+++ front/index.php
@@ -2,6 +2,6 @@
 require('model.php');
 
 //$stations = getStationsByName('Alesia');
-$minutes = getNextMissions(77,1913-2238, A);
+$minutes = getNextMissions('77','1913-2238', 'A');
 
 require('view.php');
