3a6d52cf529f83514c9eac552aeeb7eda7c000d3
Amine <sghir.ma@gmail.com>
2018-07-16 14:37:07 +0200

    added view

--- front/model.php
+++ front/model.php
@@ -9,7 +9,7 @@ function getStationsByName($stationName)
 
 function getNextMissions($lineId, $stationId, $sens)
 {
-    $response = file_get_contents("https://ezratp.sghir.me/api/arduino/nextMissions?lineId=' . $lineId . '&stationId=" . $stationId . '&sens=' . $sens);
+    $response = file_get_contents("https://ezratp.sghir.me/api/nextMissions?lineId=' . $lineId . '&stationId=" . $stationId . '&sens=' . $sens);
     echo $response;
     $response = json_decode($response);
     return $response;
