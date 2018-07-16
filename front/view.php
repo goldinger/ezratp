aef0be6e8fbb027aa7b3325670667ffd70208369
Amine <sghir.ma@gmail.com>
2018-07-16 18:01:52 +0200

    added view

--- front/view.php
+++ front/view.php
@@ -7,16 +7,21 @@
 <body>
 <h1>Easy Ratp</h1>
 <p>Stations d'alesia</p>
-
+<form action="/action_page.php">
+    <select name="stations" size="10">
 <?php
 foreach ($stations as $station)
 {
 ?>
-        <p><?php echo $station->id; ?> - <?php echo $station->name; ?>
-            <br><?php echo $station->line->id?> - <?php echo $station->line->reseau->name; ?> <?php echo $station->line->code; ?>
-        </p>
+    <option value="<?php echo $station->id; ?>;<?php echo $station->line->id?>">
+         <?php echo $station->name; ?> | <?php echo $station->line->reseau->name; ?> <?php echo $station->line->code; ?>
+    </option>
 <?php
 }
 ?>
+    </select>
+    <br><br>
+    <input type="submit">
+</form>
 </body>
 </html>
