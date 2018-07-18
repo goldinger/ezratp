33277ae723e553aaaa157b09704809f69d0b892b
Amine <sghir.ma@gmail.com>
2018-07-18 16:43:24 +0200

    added view

--- front/stations.php
+++ front/stations.php
@@ -3,6 +3,6 @@ require('model.php');
 
 $data = $_GET["data"];
 $stationName = $data;
-$stations = getStationsByName(str_replace('+', ' ', $stationName));
+$stations = getStationsByName($stationName);
 
 require('station_view.php');
