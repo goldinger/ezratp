a6057413b2c9b1558899a45bd775490ad85cbdf5
Amine <sghir.ma@gmail.com>
2018-07-23 15:10:38 +0200

    added view

--- front/model.php
+++ front/model.php
@@ -13,8 +13,8 @@ function getStationsByName($stationName)
 function getNextMissions($lineId, $stationName, $sens)
 {
     global $base_url;
-    echo $base_url . "arduino/nextMissions?lineId=" . $lineId . "&stationName=" . $stationName . "&sens=" . $sens;
-    $response = file_get_contents($base_url . "arduino/nextMissions?lineId=" . $lineId . "&stationName=" . $stationName . "&sens=" . $sens);
+//    echo $base_url . "arduino/nextMissions?lineId=" . $lineId . "&stationName=" . str_replace(' ', '+', $stationName) . "&sens=" . $sens;
+    $response = file_get_contents($base_url . "arduino/nextMissions?lineId=" . $lineId . "&stationName=" . str_replace(' ', '+', $stationName) . "&sens=" . $sens);
     $response = json_decode($response);
     return $response;
 }
